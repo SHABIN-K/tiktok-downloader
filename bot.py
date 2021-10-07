1adb56bd8d07f593defbdba5386139de3d9de67c
Shabin-k <73272797+SHABIN-K@users.noreply.github.com>
2021-10-07 20:22:37 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ from configs import Config
 from pyrogram.errors import UserNotParticipant
 from pyrogram.errors import UserBannedInChannel
 from pyrogram.errors import UsernameInvalid, UsernameNotOccupied
-from core.forcesub import forcesub
 from database.sql import add_user
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
